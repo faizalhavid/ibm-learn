21284395adb1a87d5b1d6bbeb876c3488232c8f7
faizalhavid <nurhavid123@gmail.com>
2025-05-14 23:23:48 +0700

    Enhance Dockerfile and main.go with health check and argument instructions; update bash script for environment variable handling

--- devops/docker/golang-serve/bash.sh
+++ devops/docker/golang-serve/bash.sh
@@ -4,3 +4,7 @@ docker build -t golang-server:latest golang-serve;docker image inspect golang-se
 # replace env variable
 docker container stop container-golang-server;docker container rm container-golang-server;docker rmi golang-server:latest
 docker build -t golang-server:latest golang-serve;docker image inspect golang-server:latest; docker container create --name container-golang-server --publish 8081:8081 --env APP_PORT=8081 golang-server:latest; docker container start container-golang-server; docker container logs container-golang-server;docker start container-golang-server;docker container logs container-golang-server;
+
+# command for arg instuction
+docker container stop container-golang-server;docker container rm container-golang-server;docker rmi golang-server:latest
+docker build -t golang-server:latest golang-serve --build-arg app=test ;docker image inspect golang-server:latest; docker container create --name container-golang-server --publish 8081:8081 golang-server:latest; docker container start container-golang-server; docker container logs container-golang-server;docker start container-golang-server;docker container logs container-golang-server;
